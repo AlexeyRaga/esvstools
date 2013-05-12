ec35d92bdd22e134fa37b2546151b44a179a362b
Alexey Raga <alexey.raga@gmail.com>
2013-05-12 23:39:11 +1000

    Tool window opens

--- EventStore.VSTools/MyToolWindow.cs
+++ EventStore.VSTools/MyToolWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using EventStore.VSTools.Forms;
 using Microsoft.VisualStudio.Shell;
 
 namespace EventStore.VSTools
@@ -34,7 +35,7 @@ namespace EventStore.VSTools
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on 
             // the object returned by the Content property.
-            //base.Content = new MyControl();
+            base.Content = new QueryView();
         }
     }
 }
